0a3051bf233a99424af36c27c2d4815115114cea
Atif-Nayar-Ansari <atifnayaransari999@gmail.com>
2020-02-09 01:06:03 +0530

    newvar is added in Fees class

--- src/com/atif/fees/Fees.java
+++ src/com/atif/fees/Fees.java
@@ -1,6 +1,23 @@
 package com.atif.fees;
 
 public class Fees {
+	
 	private int dfees;
+	private String cash;
+	
+	
+	public int getDfees() {
+		return dfees;
+	}
+	public void setDfees(int dfees) {
+		this.dfees = dfees;
+	}
+	public String getCash() {
+		return cash;
+	}
+	public void setCash(String cash) {
+		this.cash = cash;
+	}
+	
 
 }
