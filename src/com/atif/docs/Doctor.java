d79c1c3df35fe9607727c9303f4b0b5a15e13399
Atif-Nayar-Ansari <atifnayaransari999@gmail.com>
2020-02-09 00:41:04 +0530

    commit 1 by owner

---
+++ src/com/atif/docs/Doctor.java
@@ -0,0 +1,29 @@
+package com.atif.docs;
+
+public class Doctor {
+
+
+	private int no;
+	private String name;
+	private String place;
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+}
